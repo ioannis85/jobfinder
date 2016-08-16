c53df89f7e825153860f62844c8ecb6106a85333
ioannis <ioannis_85@hotmail.com>
2016-08-15 19:23:27 -0600

    add start script in package.json

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('*',(req,res) => {
 res.render('index');
 });
 
-const port = process.env.port || 3000;
+const port = process.env.PORT || 3000;
 const host = process.env.IP || '127.0.0.1';
 
 app.listen(port,host,function(){
